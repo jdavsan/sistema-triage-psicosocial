79acfeee79592e5cb1f691f7ffead4aa99086f10
jdavsan <jdavsan007@gmail.com>
2025-11-19 21:14:07 -0500

    Add automatic superuser creation

---
+++ management/commands/create_superuser.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Crea un superusuario si no existe'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@ejemplo.com',
+                password='admin13**'
+            )
+            self.stdout.write(self.style.SUCCESS('Superusuario creado exitosamente'))
+        else:
+            self.stdout.write(self.style.WARNING('El superusuario ya existe'))
